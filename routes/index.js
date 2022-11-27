1537524911d1aaa162111dc57c9fc00e47b58207
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-11-27 22:56:58 +0530

    Adding user routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const homeController = require('../controllers/home_controller');
 console.log("router loaded");
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
 module.exports = router;
