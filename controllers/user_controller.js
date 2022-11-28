1779d7c48118d3e76e8cbc82871b5e02b5096304
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-11-28 12:10:38 +0530

    Created file for user sign up and sign in

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,27 @@
 module.exports.profile = function(req,res){
     return res.end('<h1>User Profile</h1>');
 }
+
+// render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: 'Codial | Sign Up'
+    })
+}
+
+// render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: 'Codial | Sign In'
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req,res){
+    // Todo later
+}
+
+// Sign in and create the session
+module.exports.createSession = function(req,res){
+    // Todo later
+}
