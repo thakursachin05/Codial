d5e981492668bf7339642d842c13a2cb5758f158
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-11-29 17:48:18 +0530

    created controller for user signup and signin

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res){
     // return res.end('<h1> Jai Shree Ram </h1>');
+    console.log(req.cookies);
+    res.cookie('user_id','jaishreeram');
     return res.render('home',{
         title : 'Home'
     });
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/users');
+
 module.exports.profile = function(req,res){
     return res.end('<h1>User Profile</h1>');
 }
@@ -18,7 +20,24 @@ module.exports.signIn = function(req,res){
 
 // get the sign up data
 module.exports.create = function(req,res){
-    // Todo later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email : req.body.email} , function(err , user){
+        if(err){
+            console.log("error in finding user in signing up");
+            return;
+        }
+        if(!user){
+            User.create(req.body , function(err, user){
+                if(err){ console.log("error in creating the user while signing up"); return;}
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
 
 // Sign in and create the session
