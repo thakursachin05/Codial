254e89f18587775b8dc748b9a0e191d30ad936ba
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-11-29 17:52:20 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+// app.use(expressLayouts);
+// app.set('layout extractStyles', true);
+// app.set('layout extactScripts',  true);
 
 // use express router
 app.use('/', require('./routes'));
