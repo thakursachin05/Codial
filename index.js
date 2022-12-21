0cbdcad6da7ee511e1056369ef7aa1e774bff1c7
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-12-21 17:02:32 +0530

    updated modules

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-
 // used for session cookie and authentication
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
@@ -26,6 +26,7 @@ app.use(express.static('./assets'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// mongo store is use to store the session cookie in the db
 
 app.use(session({
     name: 'codial',
@@ -35,11 +36,22 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000*60*100)
-    }
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://localhost/codial',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 // use express router
 app.use('/', require('./routes'));
