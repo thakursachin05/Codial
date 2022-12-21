294a5aff317c540efac01d262b241902ab87f148
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-12-21 17:00:28 +0530

    updated user controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,16 @@
 const User = require('../models/users');
 
 module.exports.profile = function(req,res){
-    return res.end('<h1>User Profile</h1>');
+    return res.render('user_profile', {
+        title: 'User Profile',
+    })
 }
 
 // render the sign up page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+      return  res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title: 'Codial | Sign Up'
     })
@@ -13,6 +18,10 @@ module.exports.signUp = function(req,res){
 
 // render the sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title: 'Codial | Sign In'
     })
